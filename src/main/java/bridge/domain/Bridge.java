58f8f00c32f9ecfa0c8e79fb94430af5beddaf8d
sooyoungh <sooyoung.h8@gmail.com>
2022-12-08 16:41:28 +0900

    ✨ feat(*): 입력 숫자별 다리 방향 반환하는 기능 구현

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,20 @@
+package bridge.domain;
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+
+import java.util.List;
+
+public class Bridge {
+
+    private List<String> bridge;
+
+    public List<String> createRandomBridge(int size) {
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        bridge =  bridgeMaker.makeBridge(size);
+        return bridge;
+    }
+}
